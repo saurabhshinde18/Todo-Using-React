3ce3218a609b09f5ed1da7e53aed9dd9fa5c0146
Saurabh shinde <117251508+saurabhshinde18@users.noreply.github.com>
2024-06-23 19:18:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173, // Use the PORT environment variable or default to 5173
+    host: true, // Allows Vite to use the --host flag for network access
+  }
+});
